b2af9890e64b3710241bfcaa28f96d841a4eccc2
therulerplayback <ivanbg.cyber.2004@gmail.com>
2024-05-30 18:17:26 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+   name='pythonCheatsheet',
+   version='0.1',
+   packages=['pipenv',],
+   license='MIT',
+   long_description=open('README.txt').read(),
+)
