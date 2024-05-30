216fdd73fddc603f0d7f795851ff8f73e1bb5697
Oblivion Forgotten <124943966+0bl1v10nf0rg0773n@users.noreply.github.com>
2024-05-30 18:35:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,29 +7,34 @@ with open('README.md', 'r', encoding='utf-8') as f:
 setup(
     name='pythonCheatsheet',
     version='0.1',
-    author='therulerplayback',
+    author='The Ruler Playback',
     author_email='the@ruler.com',
-    description='A Python cheatsheet package for The Ruler',
+    description='A comprehensive Python cheatsheet package for developers by The Ruler',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/therulerplayback/pythonCheatsheet',
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    keywords='python cheatsheet utilities',
+    keywords='python cheatsheet utilities development',
     project_urls={
         'Bug Reports': 'https://github.com/therulerplayback/pythonCheatsheet/issues',
         'Source': 'https://github.com/therulerplayback/pythonCheatsheet',
+        'Documentation': 'https://github.com/therulerplayback/pythonCheatsheet/blob/main/README.md',
     },
     python_requires='>=3.6',
     license='MIT',
 )
+
+# Updated stuff...
